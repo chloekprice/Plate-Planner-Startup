d8ec954e7d3f12876e451209aa8ca43f0b3449cd
Chloe Price <ckp34@byu.edu>
2023-12-08 16:36:13 -0700

    Added service directory with backend files

---
+++ service/database.js
@@ -0,0 +1,55 @@
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const dbName = 'startup';
+const colName = 'grocery_lists';
+const listCollection = client.db('startup').collection('grocery_lists');
+
+(async function testConnection() {
+    await client.connect();
+    await client.db('startup').command({ ping: 1 });
+}) ().catch((ex) => {
+    console.log(`Unable to connect to the database with ${url} because ${ex.message}`);
+    process.exit(1);
+});
+
+async function saveList(name, list){
+    const result = await client.db(dbName).collection(colName).updateOne({userName: name}, {$set: {groceryList: list}});
+    return result;
+}
+
+async function getShoppingList(name) {
+    return listCollection.findOne({userName:name});
+}
+
+async function clearList(name){
+    const result = await client.db(dbName).collection(colName).updateOne({userName: name}, {$unset: {groceryList: ''}});
+    return result;
+}
+
+async function createUserProfile(name, email, password) {
+    // Hash the password before we insert it into the database
+    const passwordHash = await bcrypt.hash(password, 10);
+    const user = {
+        userName: name,
+        email: email,
+        password: passwordHash,
+        token: uuid.v4(),
+    };
+    const success = await client.db(dbName).collection(colName).insertOne(user);
+    return success;
+} 
+
+function getUser(check) {
+    return client.db(dbName).collection(colName).findOne(check);
+  }
+
+function checkUser(email) {
+    return client.db(dbName).collection(colName).findOne({ email: email }); 
+}
+
+module.exports = { saveList, getShoppingList, createUserProfile, clearList, getUser, checkUser };
---
+++ service/index.js
@@ -0,0 +1,120 @@
+const express = require('express');
+const app = express();
+const DB = require('./database.js');
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
+const authCookieName = 'token';
+const { peerProxy } = require('./peerProxy.js');
+
+
+// The service port. In production the frontend code is statically hosted by the service on the same port.
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
+
+// Use the cookie parser middleware for tracking authentication tokens
+app.use(cookieParser());
+
+// JSON body parsing using built-in middleware
+app.use(express.json());
+
+// Serve up the frontend static content hosting
+app.use(express.static('public'));
+
+// Trust headers that are forwarded from the proxy so we can determine IP addresses
+app.set('trust proxy', true);
+
+// Router for service endpoints
+var apiRouter = express.Router();
+app.use('/api', apiRouter);
+
+apiRouter.get('/', (req, res) => {
+  res.send("welcome to plate planner!");
+});
+
+// Create user with authentification
+apiRouter.post('/auth/create', async (req, res) => {
+  console.log("creating a new user");
+  if (await DB.checkUser(req.body.email)) {
+    res.status(409).send({ msg: 'Existing user' });
+  } else {
+    const user = await DB.createUserProfile(req.body.userName, req.body.email, req.body.password);
+    // Set the cookie
+    setAuthCookie(res, user.token);
+    res.send({
+      id: user._id,
+    });
+  }
+});
+
+// Get auth token for the provided credentials
+apiRouter.post('/auth/login', async (req, res) => {
+  console.log("logging in");
+  const user = await DB.checkUser(req.body.email);
+  if (user) {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      setAuthCookie(res, user.token);
+      res.send({ id: user._id });
+      return;
+    }
+  }
+  res.status(401).send({ msg: 'Unauthorized' });
+});
+
+// Delete auth token if stored in cookie
+apiRouter.delete('/auth/logout', (_req, res) => {
+  res.clearCookie(authCookieName);
+  res.status(204).end();
+});
+
+// Check user authorization
+apiRouter.get('/user/me', async (req, res) => {
+  authToken = req.cookies['token'];
+  const user = await DB.getUser({ token: authToken });
+  if (user) {
+    res.send({ userName: user.userName });
+    return;
+  }
+  res.status(401).send({ msg: 'Unauthorized' });
+});
+
+// Get grocery list
+apiRouter.get('/grocery_list', async (req, res) => {
+  let user = req.query.name;
+  const userList = await DB.getShoppingList(user);
+  res.send(userList.groceryList);
+});
+
+// Save grocery list
+apiRouter.post('/save_list', async (req, res) => {
+  let userInfo = req.body;
+  let name = userInfo['userName'];
+  let list = userInfo['userList'];
+  const save = await DB.saveList(name, list);
+  res.send(save);
+});
+
+// Clear grocery list
+apiRouter.post('/clear_list', async (req, res) => {
+  let userInfo = req.body;
+  let name = userInfo['userName'];
+  const clear = await DB.clearList(name);
+  res.send(clear);
+});
+
+function setAuthCookie(res, authToken) {
+  res.cookie(authCookieName, authToken, {
+    secure: true,
+    httpOnly: true,
+    sameSite: 'strict',
+  });
+}
+
+// Return the application's default page if the path is unknown
+app.use((_req, res) => {
+  res.sendFile('index.html', { root: 'public' });
+});
+
+const httpService = app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+peerProxy(httpService);
