b133e58aaf733dd9a9508ebefd64e4597730bd1e
Chloe Price <ckp34@byu.edu>
2023-12-12 17:06:26 -0700

    Added styling and format for login page

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import './login.css';
 
 export function Login() {
   return (
-    <main>
-      <div>login displayed here</div>
-    </main>
+    <section className="slide">
+      <main>
+          <div className="login">
+            <div className="box">
+              <h2>Welcome</h2>
+              <p>Login to Plan Your Plate!</p>
+              <form>
+                <div className="mb-3">
+                  <label for="exampleInputName1" className="form-label">Name</label>
+                  <input type="name" className="form-control" id="exampleInputName1" aria-describedby="emailHelp"/>
+                </div>
+                <div className="mb-3">
+                  <label for="exampleInputEmail1" className="form-label">Email address</label>
+                  <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                </div>
+                <div className="mb-3">
+                  <label for="exampleInputPassword1" className="form-label">Password</label>
+                  <input type="password" className="form-control" id="exampleInputPassword1"/>
+                </div>
+                <div className='btn-controls'>
+                  <Button type="submit" className="btn btn-dark">Login</Button>
+                  <Button type="submit" className="btn btn-dark">Create</Button>
+                </div>
+              </form>
+            </div>
+        </div> 
+      </main>
+    </section>
   );
 }
